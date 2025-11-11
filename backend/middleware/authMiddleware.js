6dfcfec8f6c6a4fd508eb729d64880f76611a6b2
zara-rad <zahra.rafieirad1980@gmail.com>
2025-11-11 13:01:21 +0100

    clean up codes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -38,33 +38,3 @@ export const protect = async (req, res, next) => {
 };
 
 
-// import jwt from "jsonwebtoken";
-// import User from "../models/User.js";
-
-// export const protect = async (req, res, next) => {
-//   let token = null;
-
-//   // Expect header: Authorization: Bearer <token>
-//   if (
-//     req.headers.authorization &&
-//     req.headers.authorization.startsWith("Bearer ")
-//   ) {
-//     token = req.headers.authorization.split(" ")[1];
-//   }
-
-//   if (!token)
-//     return res.status(401).json({ message: "Not authorized, token missing" });
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     // optionally attach user (without password)
-//     const user = await User.findById(decoded.id).select("-password");
-//     if (!user) return res.status(401).json({ message: "User not found" });
-
-//     req.user = user;
-//     next();
-//   } catch (err) {
-//     console.error("JWT verify error:", err);
-//     return res.status(401).json({ message: "Token invalid" });
-//   }
-// };
