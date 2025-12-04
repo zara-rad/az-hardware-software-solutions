0d65142f16b24f7e416f1e1797f70be536e06516
zara-rad <zahra.rafieirad1980@gmail.com>
2025-12-04 11:12:29 +0100

    Update frontend: API_BASE changes and contact form fixes

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
-export const API_BASE = "https://az-hardware-software-solutions.onrender.com";
+//export const API_BASE = "https://az-hardware-software-solutions.onrender.com";
+export const API_BASE = "http://localhost:5050";
