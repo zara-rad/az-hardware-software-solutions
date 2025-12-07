ce10d2bf1569dc0817c293b62f2c1114e44f979d
zara-rad <zahra.rafieirad1980@gmail.com>
2025-12-07 10:15:20 +0100

    Update API_BASE for VPS deployment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
 //export const API_BASE = "https://az-hardware-software-solutions.onrender.com";
-export const API_BASE = "http://localhost:5050";
+//export const API_BASE = "http://localhost:5050";
+export const API_BASE = "https://api.aqbitz.de";
